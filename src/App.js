a0a72ae51954d7f45987be4b40a593f10af9bdd0
rennehir <git@rennehir.fi>
2019-02-22 15:22:35 +0200

    Make responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import PieceOfSwot from './components/PieceOfSwot';
 
 import './App.css';
@@ -10,17 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Grid container spacing={16} xs={8} style={{ margin: 'auto', marginTop: '8rem' }}>
-          <Grid item xs={6}>
+        <Grid container spacing={16} xs={11} md={8} style={{ margin: 'auto', marginTop: '8rem' }}>
+          <Grid item xs={12} sm={6}>
             <PieceOfSwot title='Strengths' />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <PieceOfSwot title='Weaknesses' />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <PieceOfSwot title='Opportunities' />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6}>
             <PieceOfSwot title='Threats' />
           </Grid>
         </Grid>
